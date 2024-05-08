b177d672428c4b2a8179c21e6cfd418c69897c5b
Slava <steschenko.vyacheslav@gmail.com>
2024-05-08 16:19:44 +0200

    🎨 Designing models
    Currently studying designing models on Django and  learning auth solution for this framework

--- back-end/ChewChampionBackend/burger/models.py
+++ back-end/ChewChampionBackend/burger/models.py
@@ -1,80 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
-# Custom User Manager for handling user creation
-class CustomUserManager(BaseUserManager):
-    def create_user(self, email, username, password=None, role='user'):
-        if not email:
-            raise ValueError('Users must have an email address')
-        if not username:
-            raise ValueError('Users must have a username')
-
-        user = self.model(
-            email=self.normalize_email(email),
-            username=username,
-            role=role
-        )
-
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, email, username, password):
-        user = self.create_user(
-            email=self.normalize_email(email),
-            username=username,
-            password=password,
-            role='admin'
-        )
-        user.is_admin = True
-        user.save(using=self._db)
-        return user
-
-# User model
-class User(AbstractBaseUser):
+#Currently discovering designing models and authentication solution
+class Users(models.Model):
     user_id = models.AutoField(primary_key=True)
-    email = models.EmailField(verbose_name='email address', max_length=255, unique=True)
-    username = models.CharField(max_length=30, unique=True)
-    role = models.CharField(max_length=30, default='user')
-    is_active = models.BooleanField(default=True)
-    is_admin = models.BooleanField(default=False)
-
-    objects = CustomUserManager()
-
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
-
-    def __str__(self):
-        return self.username
-
-    def has_perm(self, perm, obj=None):
-        return True
-
-    def has_module_perms(self, app_label):
-        return True
-
-    @property
-    def is_staff(self):
-        return self.is_admin
-
-# Post model
-class Post(models.Model):
-    post_id = models.AutoField(primary_key=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length=255)
-    content = models.TextField()
-    date = models.DateTimeField(auto_now_add=True)
-    likes = models.IntegerField(default=0)
-
-    def __str__(self):
-        return self.title
-
-# Comment model
-class Comment(models.Model):
-    comment_id = models.AutoField(primary_key=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    content = models.TextField()
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.content
+    email = models.EmailField(max_length=254)
+    password = models.CharField(max_length=254)
+    username = models.CharField(max_length=30)
+    role = models.BooleanField(default=False)
