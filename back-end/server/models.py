32f59a58eedaacffb8fe91456f5ef50599a3f373
Slava <steschenko.vyacheslav@gmail.com>
2024-05-17 12:39:33 +0200

    Refactored models. Switched to class viewsets.

--- back-end/server/models.py
+++ back-end/server/models.py
@@ -2,16 +2,16 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Posts(models.Model):
-    post_id = models.AutoField(primary_key=True)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     content = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
     likes = models.IntegerField(default=0)
 
 class Comments(models.Model):
-    comment_id = models.AutoField(primary_key=True)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
-    post_id = models.ForeignKey(Posts, on_delete=models.CASCADE)
+    post = models.ForeignKey(Posts, on_delete=models.CASCADE)
--- back-end/server/views.py
+++ back-end/server/views.py
@@ -1,79 +1,90 @@
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
 from .serializers import UserSerializer, PostsSerializer, CommentsSerializer
 from rest_framework import status
 from rest_framework.authtoken.models import Token
 from django.contrib.auth.models import User
 from .models import Posts, Comments
-
 from django.shortcuts import get_object_or_404
-
-from rest_framework.decorators import api_view, authentication_classes, permission_classes
-from rest_framework.authentication import SessionAuthentication, TokenAuthentication
+from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework import viewsets
 
-@api_view(['POST'])
-def login(request):
-    user = get_object_or_404(User, username=request.data['username'])
-    if not user.check_password(request.data['password']):
-        return Response({'detail':'Not found.'}, status=status.HTTP_400_BAD_REQUEST)
-    token,  created = Token.objects.get_or_create(user=user)
-    serializer = UserSerializer(instance=user)
-    return Response({"token": token.key, "user": serializer.data})
+class LoginView(APIView):
+    def post(self, request):
+        user = get_object_or_404(User, username=request.data['username'])
+        if not user.check_password(request.data['password']):
+            return Response({'detail':'incorrect password.'}, status=status.HTTP_400_BAD_REQUEST)
+        token,  created = Token.objects.get_or_create(user=user)
+        serializer = UserSerializer(instance=user)
+        return Response({"token": token.key, "user": serializer.data})
 
-@api_view(['POST'])
-def signup(request):
-    serializer = UserSerializer(data=request.data)
-    if serializer.is_valid():
-        user = serializer.save()
-        user = User.objects.get(username=request.data['username'])
-        user.set_password(request.data['password'])
-        user.save()
-        token = Token.objects.create(user=user)
-        return Response({'token': token.key, 'user': serializer.data})
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class SignupView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            user = User.objects.get(username=request.data['username'])
+            user.set_password(request.data['password'])
+            user.save()
+            token = Token.objects.create(user=user)
+            return Response({'token': token.key, 'user': serializer.data})
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET'])
-@authentication_classes([SessionAuthentication, TokenAuthentication])
-@permission_classes([IsAuthenticated])
-def test_token(request):
-    return Response("passed for {}".format(request.user.email))
+class TestTokenView(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        print(request)
+        return Response("passed for {}".format(request.user.email))
+
+class HelloView(APIView):
+    def get(self, request):
+        return Response({"message" : "Hello, world!"})
+
+class PrivetView(APIView):
+    def get(self, request):
+        return Response({"сообщение" : "Привет, мир!"})
+
+class PostsViewSet(viewsets.ModelViewSet):
+    queryset = Posts.objects.all()
+    serializer_class = PostsSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    # @action(detail=False, methods=['GET'])
+    def get_queryset(self):
+        user = self.request.user
+        return Posts.objects.filter(user_id=user)
+    
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
+    
+    def create(self, request):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            user = User.objects.get(id=request.user.id)
+            serializer.save(user=user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
 
 
-@api_view(['GET'])
-def hello(request):
-    return Response({"message" : "Hello, world!"})
+class CommentsViewSet(viewsets.ModelViewSet):
+    queryset = Comments.objects.all()
+    serializer_class = CommentsSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
 
-@api_view(['GET'])
-def privet(request):
-    return Response({"сообщение" : "Привет, мир!"})
-
-@api_view(['GET'])
-def get_posts(request):
-    posts = Posts.objects.all()
-    serializer = PostsSerializer(posts, many=True)
-    return Response(serializer.data)
-
-@api_view(['POST'])
-@authentication_classes([TokenAuthentication])
-@permission_classes([IsAuthenticated])
-def create_post(request):
-    serializer = PostsSerializer(data=request.data)
-    if serializer.is_valid():
-        user = User.objects.get(id=request.user.id)
-        serializer.save(user_id=user)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-@api_view(['POST'])
-@authentication_classes([TokenAuthentication])
-@permission_classes([IsAuthenticated])
-def create_comment(request):
-    serializer = CommentsSerializer(data=request.data)
-    if serializer.is_valid():
-        user = User.objects.get(id=request.user.id)
-        post = Posts.objects.get(post_id=request.data['post_id'])
-        serializer.save(user_id=user, post_id=post)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def perform_create(self, serializer):
+        user = User.objects.get(id=self.request.user.id)
+        post = Posts.objects.get(post=self.request.data['post_id'])
+        serializer.save(user=user, post=post)
