c3e89539ee6c8d854b1617421c2108d8def2e171
Slava <steschenko.vyacheslav@gmail.com>
2024-05-21 16:41:52 +0200

    Added likes

--- back-end/server/serializers.py
+++ back-end/server/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Posts, Comments
+from .models import Posts, Comments, Like
 from django.contrib.auth.models import User
 
 class UserSerializer(serializers.ModelSerializer):
@@ -9,13 +9,20 @@ class UserSerializer(serializers.ModelSerializer):
 
 class PostsSerializer(serializers.ModelSerializer):
     user = serializers.PrimaryKeyRelatedField(read_only=True)
+    username = serializers.ReadOnlyField(source='user.username')
     class Meta:
         model = Posts
-        fields = ['user','post', 'title', 'content', 'image_url']
+        fields = ['user','username', 'post', 'title', 'content', 'image_url']
 
 class CommentsSerializer(serializers.ModelSerializer):
     user = serializers.PrimaryKeyRelatedField(read_only=True)
     post = serializers.PrimaryKeyRelatedField(read_only=True)
+    username = serializers.ReadOnlyField(source='user.username')
     class Meta:
         model = Comments
-        fields = ['comment', 'user', 'content', 'post', 'date']
+        fields = ['comment', 'user', 'username', 'content', 'post', 'date']
+    
+class LikeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Like
+        fields = ['user', 'post']
--- back-end/server/urls.py
+++ back-end/server/urls.py
@@ -1,11 +1,13 @@
 from django.urls import re_path, path, include
 from . import views
-from server.views import LoginView, SignupView, TestTokenView, HelloView, PrivetView, PostsViewSet, CommentsViewSet
+from server.views import LoginView, SignupView, TestTokenView, HelloView, PrivetView, PostsViewSet, CommentsViewSet, UserViewSet, LikeViewSet
 from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register(r'posts', PostsViewSet)
 router.register(r'comments', CommentsViewSet)
+router.register(r'users', UserViewSet)
+router.register(r'likes', LikeViewSet)
 
 urlpatterns = [
     path('hello/', HelloView.as_view(), name='hello'),
@@ -13,5 +15,5 @@ urlpatterns = [
     path('signup/', SignupView.as_view(), name='signup'),
     path('test_token/', TestTokenView.as_view(), name='test_token'),
     path('', include(router.urls)),
-    path('privet/', PrivetView.as_view(), name='privet')
+    path('privet/', PrivetView.as_view(), name='privet'),
 ]
